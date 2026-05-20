5fcbb59d78a4c365a3cec3824ce2027a7687e933
Zedekiah Heteroza <zedekiahheteroza@gmail.com>
2026-05-20 22:46:31 +0800

    Add lower administrative divisions

--- src/psgc.php
+++ src/psgc.php
@@ -1,11 +1,16 @@
 <?php
     header("Content-Type: application/json");
-    require 'vendor/autoload.php';
+    require '../vendor/autoload.php';
 
     use Rootscratch\PSGC\PSGC;
 
     $psgcApi = new PSGC();
+
     $regions = $psgcApi->Regions();
+    $provinces = $psgcApi->Provinces();
+    $municities = $psgcApi->MunicipalAndCities();
+    $municipalities = $psgcApi->Municipal();
+    $cities = $psgcApi->City();
     $psgcRegion = "Region I";
-    // echo json_encode($regions, JSON_PRETTY_PRINT);
+    // echo json_encode($provinces, JSON_PRETTY_PRINT);
 ?>
